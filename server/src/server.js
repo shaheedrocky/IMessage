0c2fca1a1c9a1d3de856975f82bfb0d8f339e94f
shaheedrocky <shaheedsibil@gmail.com>
2026-06-13 17:20:04 +0530

    add dockerfile and dockerIgnore file, implemented express server with static file serving

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import "dotenv/config.js";
 import connectDB from "./lib/db.js";
 import dns from "dns";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
+import fs from "fs";
+import path from "path";
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
@@ -11,10 +13,18 @@ const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
-app.use(express.json())
-app.use(cors({origin: FRONTEND_URL, credentials: true}))
-app.use(clerkMiddleware())
+const publicDir = path.join(process.cwd(), "public");
 
+app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(clerkMiddleware());
+
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
