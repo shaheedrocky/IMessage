7ea597b436b7464bb3ce4e17cfc747260bad8542
shaheedrocky <shaheedsibil@gmail.com>
2026-06-14 12:46:40 +0530

    webhook clerk added

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ import { clerkMiddleware } from "@clerk/express";
 import fs from "fs";
 import path from "path";
 import { job } from "./lib/cron.js";
-
+import clerkWebhook from './webhooks/clerk.webhook.js'
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
 
@@ -15,6 +15,8 @@ const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+app.use('/api/webhooks/clerk', express.raw({type: 'application/json'}), clerkWebhook)
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
